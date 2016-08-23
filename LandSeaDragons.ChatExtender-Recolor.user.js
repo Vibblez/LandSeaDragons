12ac1e26d382b67c74829eef76b889609d29f536
Christopher Jones <chris.jones@level-b.co.uk>
2016-08-23 12:42:09 +0100

    Initial commit

---
+++ LandSeaDragons.ChatExtender-Recolor.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Land Sea Dragons - Chat Extender & Recolor
+// @namespace    https://github.com/Vibblez/MidenQuestStyle
+// @version      0.1.0
+// @description  MidenQuest Chat Extender
+// @updateURL    https://raw.githubusercontent.com/Vibblez/LandSeaDragons/master/LandSeaDragons.ChatExtender-Recolor.user.js
+// @author       Vibblez
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
+// @include      https://www.lsdrpg.com/game.php
+// @include      https://lsdrpg.com/game.php
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+    }
+
+    addGlobalStyle('#chat_main_border { height: 400px; } #chat_main_border > tbody > tr > td > div { height: 480px !important; } .chat_1, .chat_2 { color: #f148e2; }');
+
+})();
