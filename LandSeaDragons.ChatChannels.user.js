543c15e6366e870391e4611dfe53be95b9e24063
Chris Jones <chris.jones@level-b.co.uk>
2016-08-24 13:04:27 +0100

    Added filter to chat channels

--- LandSeaDragons.ChatChannels.user.js
+++ LandSeaDragons.ChatChannels.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Land Sea Dragons - Chat Channels
 // @namespace    https://github.com/Vibblez/LandSeaDragons
-// @version      0.1.2
+// @version      0.1.3
 // @description  Chat Channel Drop Down
 // @updateURL    https://raw.githubusercontent.com/Vibblez/LandSeaDragons/master/LandSeaDragons.ChatChannels.user.js
-// @author       Vibblez
+// @author       Vibblez, euloghtos
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
 // @include      https://www.lsdrpg.com/game.php
 // @include      https://lsdrpg.com/game.php
@@ -14,15 +14,44 @@
 (function() {
     'use strict';
 
-    $("#chat_form > tbody > tr").html('<tr><td><select id="chat_channel"><option value="">Main</option><option value="/f ">Fleet</option></select><input id="chat_input" type="text" maxlength="200" autocomplete="off"></td><td style="text-align:right;"><input id="chat_submit" type="submit" value="Chat"></td></tr>');
+    $("#chat_form > tbody > tr").html('<tr><td><select id="chat_channel"><option value="">Public</option><option value="/f ">Fleet</option></select><input id="chat_input" type="text" maxlength="200" autocomplete="off"></td><td style="text-align:right;"><input id="chat_submit" type="submit" value="Chat"> Filter: <select id="chatSelect" style="margin-right: 5px"><option value="">All</option><option value="msgMain">Main</option><option value="msgPM">PM</option><option value="msgFleet">Fleet</option></select></td></tr>');
     
+	/////////////////
+	/// Filter started by: euloghtos, completed by Vibblez
+	/////////////////
+    $('#chatSelect').change(function(){
+		if($('#chatSelect').val() != ""){
+			$('#chat_main tr').not('.'+$(this).val()).hide();
+			$('#chat_main tr.'+$(this).val()).show();
+		}else{
+			$('#chat_main tr').show();
+		}
+    });
+
+    $('#chat_main').on('DOMNodeInserted', 'tr', function(e) {
+        if ($('span.chat_5', e.target).length > 0) $(e.target).addClass('msgFleet');
+		if ($('span.chat_1', e.target).length > 0) $(e.target).addClass('msgPM');
+		if ($('span.chat_2', e.target).length > 0) $(e.target).addClass('msgPM');
+		if ($('span.chat_0_0_0', e.target).length > 0) $(e.target).addClass('msgMain');
+		if ($('span.chat_0_1_0', e.target).length > 0) $(e.target).addClass('msgMain');
+		if ($('span.chat_0_2_0', e.target).length > 0) $(e.target).addClass('msgMain');
+		if($('#chatSelect').val() != "")
+        if (!$(e.target).hasClass($('#chatSelect').val())) $(e.target).hide();
+    });
+	//////////////
+	/// Filter End
+	//////////////
+	
     var origGetChat = getChat;
     getChat = function (n) {
 		$("#chat_submit").prop( "disabled", true );
-		var chatBuilder;
-		chatBuilder = $('#chat_input').val();
-		var chat_cmd_check = chatBuilder.split(" ");
-		if(chat_cmd_check[0] != "/m" && chat_cmd_check[0] != "/view") chatBuilder = $('#chat_channel').val() + $('#chat_input').val();
+		var chatBuilder = "";
+		var chatInput = $('#chat_input').val();
+		var chatChannel = $('#chat_channel').val();
+		var chat_cmd_check = chatInput.split(" ");
+		if(chatInput.length && chat_cmd_check[0] != "/m" && chat_cmd_check[0] != "/view"){ chatBuilder = chatChannel + chatInput; }
+		else if(chatInput.length && chat_cmd_check[0] == "/p" && chatChannel == "/f "){ chatBuilder = chatInput.replace("/p ", ""); console.log(chatInput) }
+		else { chatBuilder = chatInput; }
         var input = 'null';
         switch(n) {
             case 1:
@@ -85,6 +114,6 @@
     head.appendChild(style);
     }
 
-    addGlobalStyle('#chat_input { width: 790px !important; }');
+    addGlobalStyle('#chat_input { width: 670px !important; } #layer_2 { z-index: 3 !important; }');
 
 })();
